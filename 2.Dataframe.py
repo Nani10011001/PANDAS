55eb69b36b86c8a61e2e16e04996789572aa2efe
NANi <nani1531323@gmail.com>
2025-10-11 22:23:57 +0530

    Update 2.Dataframe.py

--- 2.Dataframe.py
+++ 2.Dataframe.py
@@ -1,7 +1,7 @@
 import pandas as pd
 data=pd.DataFrame({
-    "name":["nani","nina","soni"],
-    "age":[24,23,22],
-    "job":["full_stack Ai devloper","data anylist","front end developer"]
+    "name":["nani","nina","soni","vani"],
+    "age":[24,23,22,25],
+    "job":["full_stack Ai devloper","data anylist","front end developer","backend devloper"]
 })
 print(data)
